a20bbadbeb52edfbed688f47eac902cc959e86cd
Justin-code2 <Justi@ANDRESMV>
2024-10-30 01:12:21 -0500

    Evalucion Completa

--- Project.WEB/Repositories/IRepository.cs
+++ Project.WEB/Repositories/IRepository.cs
@@ -1,19 +1,29 @@
-﻿namespace Project.WEB.Repositories
+﻿using System.Threading.Tasks;
+
+namespace Project.WEB.Repositories
 {
     public interface IRepository
 
     {
 
         Task<HttpResponseWrapper<T>> Get<T>(string url);
-
-
-
         Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
 
 
 
         Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);
 
+
+
+        Task<HttpResponseWrapper<object>> DeleteAsync<T>(string url);
+
+        Task<HttpResponseWrapper<object>> PutAsync<T>(string url, T model);
+
+
+
+        Task<HttpResponseWrapper<TResponse>> PutAsync<T, TResponse>(string url, T model);
+
+        
     }
 
 }
