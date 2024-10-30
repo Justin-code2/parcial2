48e2d554886f88817e966f5e73cce2c666d9ddd3
Justin-code2 <Justi@ANDRESMV>
2024-10-29 23:24:42 -0500

    Razor

---
+++ Project.WEB/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Project.WEB;
+using Project.WEB.Repositories;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7030") });
+builder.Services.AddScoped<IRepository, Repository>();
+
+await builder.Build().RunAsync();
